e723ebc10b433bffdf83925c278d9edfbad43934
ferreira-fabio <fabioricardoferreira@hotmail.com>
2022-01-22 20:19:27 +0000

    Study commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace TestingDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            System.Console.WriteLine("Testando Datas e Horários Locais");
+            System.Console.WriteLine("---------------------------------");
+            Thread.Sleep(1000);
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Sua data atual é:");
+            Thread.Sleep(500);
+            var horaAtual = DateTime.Now; //Chamando a propriedade Now de da Classe DateTime para mostrar a hora atual de acordo com o dispositivo usado.
+            System.Console.WriteLine(horaAtual); 
+            var br =  new CultureInfo("pt-BR"); //Criando um padrão conforme regiao com a instancia da classe culture info
+            
+            System.Console.WriteLine("No formato brasileiro:");
+            System.Console.WriteLine(horaAtual.ToString("g" , br)); //Formatando com ToString e usando oo objeto criado de CultureInfo
+            var horaLocal = TimeZoneInfo.FindSystemTimeZoneById("Bahia Standard Time"); //chamando o método FindSystemTimeZoneById(string id) para saber o valor utc
+            System.Console.WriteLine(horaLocal);
+            var convertHora = TimeZoneInfo.ConvertTime(horaAtual, TimeZoneInfo.Local, horaLocal);//usando o método TimeZoneInfo.ConvertTime(DateTime, TimeZoneInfo, TimeZoneInfo)) para converter a hora local 
+            System.Console.WriteLine(convertHora);                                               //em hora de determinada regiao com os parametro recebidos.
+
+           
+            /*foreach (TimeZoneInfo z in TimeZoneInfo.GetSystemTimeZones())
+            
+                Console.WriteLine(z.Id);
+            
+*/      //Laço usado para buscar o Id para o timeZoneInfo,para entao poder inserir dentro do método TimeZoneInfo.ConvertTime(DateTime, TimeZoneInfo, TimeZoneInfo).
+
+            
+
+        }
+    }
+}
